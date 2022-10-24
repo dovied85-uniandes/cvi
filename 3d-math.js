336399df50799a4be90a6bf74c991dbf91d0cb45
dovied85-uniandes <78886805+dovied85-uniandes@users.noreply.github.com>
2022-10-24 17:32:57 -0500

    Update 3d-math.js

--- 3d-math.js
+++ 3d-math.js
@@ -43,6 +43,65 @@ var rotVectores = function(u, v) {
 };
 
 // OPERACIONES PARA MATRICES
+var multiplyMatrix = function(a, b) {
+  var a00 = a[0*4+0];
+  var a01 = a[0*4+1];
+  var a02 = a[0*4+2];
+  var a03 = a[0*4+3];
+  var a10 = a[1*4+0];
+  var a11 = a[1*4+1];
+  var a12 = a[1*4+2];
+  var a13 = a[1*4+3];
+  var a20 = a[2*4+0];
+  var a21 = a[2*4+1];
+  var a22 = a[2*4+2];
+  var a23 = a[2*4+3];
+  var a30 = a[3*4+0];
+  var a31 = a[3*4+1];
+  var a32 = a[3*4+2];
+  var a33 = a[3*4+3];
+  var b00 = b[0*4+0];
+  var b01 = b[0*4+1];
+  var b02 = b[0*4+2];
+  var b03 = b[0*4+3];
+  var b10 = b[1*4+0];
+  var b11 = b[1*4+1];
+  var b12 = b[1*4+2];
+  var b13 = b[1*4+3];
+  var b20 = b[2*4+0];
+  var b21 = b[2*4+1];
+  var b22 = b[2*4+2];
+  var b23 = b[2*4+3];
+  var b30 = b[3*4+0];
+  var b31 = b[3*4+1];
+  var b32 = b[3*4+2];
+  var b33 = b[3*4+3];
+  return [a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30,
+          a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31,
+          a00 * b02 + a01 * b12 + a02 * b22 + a03 * b32,
+          a00 * b03 + a01 * b13 + a02 * b23 + a03 * b33,
+          a10 * b00 + a11 * b10 + a12 * b20 + a13 * b30,
+          a10 * b01 + a11 * b11 + a12 * b21 + a13 * b31,
+          a10 * b02 + a11 * b12 + a12 * b22 + a13 * b32,
+          a10 * b03 + a11 * b13 + a12 * b23 + a13 * b33,
+          a20 * b00 + a21 * b10 + a22 * b20 + a23 * b30,
+          a20 * b01 + a21 * b11 + a22 * b21 + a23 * b31,
+          a20 * b02 + a21 * b12 + a22 * b22 + a23 * b32,
+          a20 * b03 + a21 * b13 + a22 * b23 + a23 * b33,
+          a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30,
+          a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31,
+          a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32,
+          a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33];
+};
+var transformPoint = function(m, v) {
+  var v0 = v[0];
+  var v1 = v[1];
+  var v2 = v[2];
+  var d = v0 * m[0*4+3] + v1 * m[1*4+3] + v2 * m[2*4+3] + m[3*4+3];
+  return [(v0 * m[0*4+0] + v1 * m[1*4+0] + v2 * m[2*4+0] + m[3*4+0]) / d,
+          (v0 * m[0*4+1] + v1 * m[1*4+1] + v2 * m[2*4+1] + m[3*4+1]) / d,
+          (v0 * m[0*4+2] + v1 * m[1*4+2] + v2 * m[2*4+2] + m[3*4+2]) / d];
+};
 var inverse = function(m) {
   var tmp_0 = m[2*4+2] * m[3*4+3];
   var tmp_1 = m[3*4+2] * m[2*4+3];
@@ -165,7 +224,7 @@ var rotEjeAngulo = function(k, s, c) {
 var updateRPYFromDelta = function(RPY, dRPY) {
   var  R = srp2matrix([1,1,1],  RPY, [0,0,0]);
   var dR = srp2matrix([1,1,1], dRPY, [0,0,0]);
-  var  M = m.multiplyMatrix(dR, R);
+  var  M = multiplyMatrix(dR, R);
   var sz = -M[4];
   if (sz ==  1) {
     return [angle(RPY[2] - Math.atan2(-M[2],M[1])),  90, RPY[2]];
@@ -220,9 +279,9 @@ var srp_triangle = function(pt1, pt2, pt3) {
   var n2 = crossP(u, v);
   n2 = n2.map(e => e/norm(n2));
   var R1 = rotVectores(n1, n2);
-  var R2 = rotVectores(m.transformPoint(R1, [-1,0,0]), v);
-  var RT = m.multiplyMatrix(R1, R2);
-  var  t = delta(m.transformPoint(RT, [0,c,0]), pt1);
+  var R2 = rotVectores(transformPoint(R1, [-1,0,0]), v);
+  var RT = multiplyMatrix(R1, R2);
+  var  t = delta(transformPoint(RT, [0,c,0]), pt1);
   return [b*RT[0], b*RT[1],  b*RT[2], 0,
           c*RT[4], c*RT[5],  c*RT[6], 0,
             RT[8],   RT[9],   RT[10], 0,
